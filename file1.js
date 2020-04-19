0a9279953681abc64be3b3be0ffa2d1d0e262201
arawat <ashishrawat1911@gmail.com>
2020-04-19 13:33:41 +0530

    Git reverted

--- file1.js
+++ file1.js
@@ -3,4 +3,3 @@ console.log("Second console logged in GITHUB");
 console.log("git second test branch commit here ")
 console.log("console in Branch secondTest Branch")
 console.log("Third update in Third commit ")
-console.log("In master branch date 18/4/2020")
