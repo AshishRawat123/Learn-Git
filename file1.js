cbe8282af13997ea980fa412ef0b6576fe593b28
arawat <ashishrawat1911@gmail.com>
2020-04-19 14:02:42 +0530

    Clear file1 and file2 Data 19,April 2020 2pm

--- file1.js
+++ file1.js
@@ -1,5 +1,1 @@
-console.log("hi my first js file");
-console.log("Second console logged in GITHUB");
-console.log("git second test branch commit here ")
-console.log("console in Branch secondTest Branch")
-console.log("Third update in Third commit ")
+
--- file2.js
+++ file2.js
@@ -1,4 +1,2 @@
 console.log("1 March merge Test");
 console.log("second command to merge the commit.");
-console.log("delete branch from local 'git branch -d branchName'")
-console.log("delete branch from remote command 'git push origin --delete remoteBranchName'")
