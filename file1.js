8c9922609fea84117e30a43b1ad8b1c72ffba975
arawat <ashishrawat1911@gmail.com>
2020-04-19 14:43:12 +0530

    Add all three aomments

--- file1.js
+++ file1.js
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
 
 console.log("First file first commit 19 april 2pm")
 console.log("First file Second commit 19 april 2:06pm")
 console.log("First file Third commit 19 april 2:15pm")
+=======
+console.log("hi my first js file");
+console.log("Second console logged in GITHUB");
+console.log("git second test branch commit here ")
+console.log("console in Branch secondTest Branch")
+console.log("Third update in Third commit ")
+>>>>>>> parent of cbe8282... Clear file1 and file2 Data 19,April 2020 2pm
--- file2.js
+++ file2.js
@@ -1,2 +1,4 @@
 console.log("1 March merge Test");
 console.log("second command to merge the commit.");
+console.log("delete branch from local 'git branch -d branchName'")
+console.log("delete branch from remote command 'git push origin --delete remoteBranchName'")
